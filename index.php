ab11aee4bc0c10555292656deacfcc7fb9379b29
Sedov Dmitry <sedov.dmitry1@gmail.com>
2025-11-22 22:59:09 +0300

    typo

--- index.php
+++ index.php
@@ -356,7 +356,7 @@ declare(strict_types=1);
               <h3>Цены от 800 ₽</h3>
                 <p class="text-muted">Место в мужском номере: 800&nbsp;₽.</p>
                 <p class="text-muted">Место в женском номере: 900&nbsp;₽.</p>
-                <p class="text-muted">Семейный номер: от 2000&nbsp;₽.</p>
+                <p class="text-muted">Семейный номер: от&nbsp;2000&nbsp;₽.</p>
             </div>
           </div>
         </div>
